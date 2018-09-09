1b3386d664be55a4dd8f87b22c07702840c4cd41
Thomas Getgood <thomas.getgood@gmail.com>
2018-09-09 13:09:47 -0400

    Almost rendering.

--- src/falloleen/renderer/canvas.clj
+++ src/falloleen/renderer/canvas.clj
@@ -1,22 +1,42 @@
 (ns falloleen.renderer.canvas
   (:require [clojure.string :as string]))
 
-(defn- cmd
+(defn- cmd-name
   "Converts idomatic kebab-case to upperCamelCase for JS commands"
   [sym]
   (let [chunks (string/split (name sym) #"-")]
-    (symbol (apply str "." (first chunks) (map string/capitalize (rest chunks)))))
-  )
+    (symbol (apply str "." (first chunks) (map string/capitalize (rest chunks))))))
+
+(defn- type-case
+  "Converts idomatic kebab-case to UpperCamelCase for type names."
+  [sym]
+  (symbol (apply str (map string/capitalize (string/split (name sym) #"-")))))
+
+(defmacro defstyle [n args]
+  (let [tname (type-case n)
+        k (keyword (name n))]
+    `(do
+       (deftype ~n ~args
+         falloleen.renderer.canvas/IExec
+         (exec [this ctx# stack#]
+           (when (free? stack# ~k)
+             (~(cmd-name n) ctx# ~@args)
+             (assoc-style stack# ~k this)))
+
+          ~'IPrintWithWriter
+          (~'-pr-writer [o# w# _#]
+           (~'write-all w# (str "#StyleInstruction" o#)))))))
 
 (defmacro defcanvas
   ([n] `(defcanvas ~n []))
   ([n args]
-   (let [tname (gensym)]
+   (let [tname (type-case n)]
      `(do
         (deftype ~tname ~args
           falloleen.renderer.canvas/IExec
-          (exec [_# ctx#]
-            (~(cmd n) ctx# ~@args))
+          (exec [_# ctx# states#]
+            (~(cmd-name n) ctx# ~@args)
+            states#)
 
           ~'IPrintWithWriter
           (~'-pr-writer [o# w# _#]
@@ -24,7 +44,7 @@
 
           ~'Object
           (~'toString [_#]
-           (str "(" ~(str (cmd n)) " ctx" ~@(interleave (repeat " ") args) ")")))
+           (str "(" ~(str (cmd-name n)) " ctx" ~@(interleave (repeat " ") args) ")")))
         (def ~n
           ~(if (empty? args)
              `(new ~tname)
--- src/falloleen/renderer/canvas.cljs
+++ src/falloleen/renderer/canvas.cljs
@@ -1,27 +1,46 @@
 (ns falloleen.renderer.canvas
-  (:require [falloleen.lang :as lang])
+  (:require [falloleen.lang :as lang]
+            [falloleen.math :as math])
   (:require-macros [falloleen.renderer.canvas :refer [defcanvas]]
                    [falloleen.renderer.util :as util]))
 
 (defprotocol IExec
-  (exec [this ctx] "Execute this instruction on ctx."))
+  (exec [this ctx state] "Execute this instruction on ctx."))
 
 (defprotocol Canvas2DRenderable
   (cmds [this] "Returns a map of cmds to draw in various circumstances."))
 
-(defcanvas save)
-(defcanvas restore)
+(defn push-down [stack]
+  (cons (first stack) stack))
+
+(defn pop-frame [stack]
+  (rest stack))
+
+(defn diff-top [stack]
+  (if (< (count stack) 2)
+    (first stack)
+    (let [f0 (first stack)
+          f1 (second stack)]
+      (into {}
+            (map (fn [[k v]]
+                   (when-not (= v (get f0 k))
+                     [k v]))
+                 f1)))))
+
+(defn free? [stack p]
+  (not (contains? (first stack) p)))
+
+(defn assoc-style [stack k v]
+  (cons (assoc (first stack) k v) (rest stack)))
+
+(defn update-style [stack k f]
+  (cons (update (first stack) k f) (rest stack)))
+
 (defcanvas stroke)
 (defcanvas fill)
 (defcanvas begin-path)
 (defcanvas close-path)
 (defcanvas clip)
-
-(defcanvas fill-style [s])
-(defcanvas stroke-style [s])
-(defcanvas global-alpha [a])
-(defcanvas font [f])
-
 
 (defcanvas move-to [x y])
 (defcanvas line-to [x y])
@@ -31,9 +50,64 @@
 
 (defcanvas fill-text [text x y])
 
-(defcanvas transform [a b c d x y])
 
-(defcanvas line-width-hack [mag])
+
+(defcanvas fill-style [s])
+(defcanvas stroke-style [s])
+(defcanvas global-alpha [a])
+(defcanvas font [f])
+
+(def default-styles
+  {:fill-style   "rgba(0,0,0,0)"
+   :stroke-style "rgba(0,0,0,1)"
+   :global-alpha 1
+   :font         "sans serif 10px"
+   :transform    [1 0 0 1 0 0]})
+
+(def init-stack
+  (list {:fill-style   :default
+         :stroke-style :default
+         :global-alpha :default
+         :font         :default
+         :transform    :default}))
+
+(deftype Save []
+  IPrintWithWriter
+  (-pr-writer [_ w _]
+   (write-all w (str "#SaveState")))
+
+  IExec
+  (exec [_ _ stack]
+    (push-down stack)))
+
+(deftype Restore []
+  IPrintWithWriter
+  (-pr-writer [_ w _]
+   (write-all w (str "#RestoreState")))
+
+  IExec
+  (exec [_ ctx stack]
+    (let [diff (diff-top stack)]
+      (run! exec (vals diff))
+      (pop-frame stack))))
+
+(def save (Save.))
+(def restore (Restore.))
+
+(defcanvas translate [x y])
+
+(deftype Transform [m]
+  IPrintWithWriter
+  (-pr-writer [o w _]
+   (write-all w (str "#Transformation" m)))
+
+  IExec
+  (exec [_ ctx stack]
+    (let [[a b c d x y] m]
+      (.transform ctx a c b d x y)
+      (update-style stack :transform #(math/comp-atx % m)))))
+
+;; (defcanvas line-width-hack [mag])
 
 (util/implement-sequentials
  Canvas2DRenderable
@@ -53,13 +127,16 @@
         (println "Don't know how to render a "
                  t ". Doing nothing."))))
 
-  lang/Transformed
+  lang/TransformedShape
   (cmds [this]
-    {:draw (cmds (.-base this))})
+    {:pre [(Transform. (lang/atx this))
+           save]
+     :draw (cmds (lang/contents this))
+     :post [restore]})
 
   lang/RawText
-  (cmds [{[x y] :corner :keys [text]}]
-)
+  (cmds [{:keys [text]}]
+    (fill-text text 0 0))
 
   ;; Templates can implement protocols too
   ;; lang/Rectangle
