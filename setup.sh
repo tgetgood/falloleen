35a207b84cb6ae711d4a8a1e84ef4aa35502725f
Thomas Getgood <thomas.getgood@gmail.com>
2018-09-27 10:58:35 -0400

    Make classes dir if it doesn't exist.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env sh
 
-clj -e "(compile 'falloleen.hosts.jfx)"
+if test ! -e deps.edn
+	 then
+		 echo "You must run setup.sh from the project root"
+		 exit 1
+fi
+
+test -d classes || mkdir classes
+
+clojure -e "(compile 'falloleen.hosts.jfx)"
