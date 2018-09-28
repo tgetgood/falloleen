1d2aecbb03cafd042514c088942fa5b33b5a1555
Thomas Getgood <thomas.getgood@gmail.com>
2018-09-27 20:14:04 -0400

    This actually works. I think.

--- src/falloleen/hosts/browser_canvas.cljs
+++ src/falloleen/hosts/browser_canvas.cljs
@@ -37,19 +37,31 @@
                    (cb)))
                200))))))
 
+(def fullstyles
+  (str "overflow:hidden;"
+       "width:100%;"
+       "height:100%;"
+       "margin:0"))
+
 (defn init-canvas
   "Clears children of the main element and adds a canvas beneath it."
   [opts]
-  (let [id (get opts :id "app")
+  (let [id   (get opts :id "app")
         elem (get-elem id)
-        c (js/document.createElement "canvas")]
+        c    (js/document.createElement "canvas")]
     (unchecked-set elem "innerHTML" "")
     (.appendChild elem c)
     (let [ctx (.getContext c "2d")]
       (when-let [s (:size opts)]
-        (if (= s :fullscreen)
+        (cond
+          (= s :fullscreen)
           (do
-            (set-canvas-size! c (window-dimensions))
-            #_(set-canvas-size! c (element-dimensions id)))
-          (set-canvas-size! c s)))
+            (run! (fn [e] (.setAttribute e "style" fullstyles))
+                  [(aget (js/document.getElementsByTagName "html") 0)
+                   js/document.body
+                   elem])
+            (set-canvas-size! c (element-dimensions id)))
+
+          :fill-container (set-canvas-size! c (element-dimensions id))
+          :else           (set-canvas-size! c s)))
       {:canvas c :ctx ctx :elem elem})))
