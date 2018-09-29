53b7825a5ae48ac844d3df4ff026ee4ea31067e8
Thomas Getgood <thomas.getgood@gmail.com>
2018-09-29 12:17:59 -0400

    Starting back on the recursive decent rendering path.
    This was too slow in the browser, will it be acceptable here? Likely not, but it
    will get things off the ground.

--- src/falloleen/renderer/fx_canvas.clj
+++ src/falloleen/renderer/fx_canvas.clj
@@ -1,12 +1,42 @@
 (ns falloleen.renderer.fx-canvas
-  (:require [falloleen.renderer.canvas-compiler :as compiler])
-  (:import [javafx.scene.canvas Canvas GraphicsContext]))
+  (:require [falloleen.math :as math]
+            [falloleen.renderer.canvas-compiler :as compiler])
+  (:import javafx.scene.canvas.GraphicsContext
+           javafx.scene.shape.ArcType))
+
+(defonce tester (atom nil))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;; Inefficient and easy method
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defprotocol DirectExecutor
+  "Shapes implementing this protocol can directly draw themselves to screen in
+  recursive decent style."
+  (draw! [this ctx stack]))
+
+(extend-protocol DirectExecutor
+  Object
+  (draw! [this ctx stack]
+    (println "I don't know how to draw a " (type this))))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;; And go!
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def init-stack
+  (list {:fill      nil
+         :stroke    nil
+         :opacity   nil
+         :font      nil
+         :transform [1 0 0 1 0 0]}))
 
 (defn clear! [^GraphicsContext ctx]
   (let [canvas (.getCanvas ctx)]
     (.clearRect ctx 0 0 (.getWidth canvas) (.getHeight canvas))))
 
-(defonce tester (atom nil))
-
-(defn simple-render [shape ctx]
-  (reset! tester (compiler/cmds shape)))
+(defn simple-render [shape ^GraphicsContext ctx]
+  (clear! ctx)
+  (draw! shape ctx init-stack))
