eb76cf71d6969ab0ca5c6721624fccafefba97bf
Thomas Getgood <thomas.getgood@gmail.com>
2018-10-07 13:13:18 -0400

    Revert "Trying to use js* instead of eval."
    This reverts commit 7ef7f9a64e8eb838bdd9b5f34d2e6cfe3db08b25.

--- src/falloleen/renderer/jsc.cljs
+++ src/falloleen/renderer/jsc.cljs
@@ -5,84 +5,88 @@
 
 (defn colour [c]
   (if (= c :none)
-    "rgba(0,0,0,0)"
+    "'rgba(0,0,0,0)'"
     (if (keyword? c)
       (name c)
       c)))
 
-(defn c-style [{:keys [font stroke fill opacity]} ctx]
-  (when-not (nil? font)
-    (js* "~{}.font=~{}" ctx font))
-  (when-not (nil? opacity)
-    (js* "~{}.globalAlpha=~{}" ctx opacity))
-  (when-not (nil? stroke)
-    (js* "~{}.strokeStyle=~{}" ctx (colour stroke)))
-  (when-not (nil? fill)
-    (js* "~{}.fillStyle=~{}" ctx (colour fill))))
+(defn c-style [{:keys [font stroke fill opacity]}]
+  (str (when font
+         (str "ctx.font=" font ";"))
+       (when opacity
+         (str "ctx.globalAlpha=" opacity ";"))
+       (when stroke
+         (str "ctx.strokeStyle=" (colour stroke) ";"))
+       (when fill
+         (str "ctx.fillStyle=" (colour fill) ";"))))
 
 (defprotocol JSPathCompile
-  (path-compile [this ctx]))
+  (path-compile [this]))
 
 (defprotocol JSCompile
-  (jsc [this ctx]))
+  (jsc [this]))
 
 (extend-protocol JSPathCompile
   Line
-  (path-compile [{[x2 y2] :to} ctx]
-    (js* "~{}.lineTo(~{},~{});" ctx x2 y2))
+  (path-compile [{[x2 y2] :to}]
+    (str "ctx.lineTo(" x2"," y2 ");"))
 
   Bezier
-  (path-compile [{[cx1 cy1] :c1 [cx2 cy2] :c2 [x2 y2] :to} ctx]
-    (js* "~{}.bezierCurveTo(~{},~{},~{},~{},~{},~{});"
-         ctx cx1 cy1 cx2 cy2 x2 y2)))
+  (path-compile [{[cx1 cy1] :c1 [cx2 cy2] :c2 [x2 y2] :to}]
+    (str "ctx.bezierCurveTo(" cx1 "," cy1 "," cx2 "," cy2 "," x2 "," y2 ");")))
 
 (falloleen.util/implement-sequentials
  JSCompile
- (jsc [this ctx]
-   (run! #(jsc % ctx) this)))
+ (jsc [this]
+   (transduce (map jsc) str "" this)))
 
 (extend-protocol JSCompile
   default
-  (jsc [this ctx]
+  (jsc [this]
     (if (lang/template? this)
-      (jsc (lang/expand-template this) ctx)
+      (jsc (lang/expand-template this))
       (println "Don't know how to render a" (type this))))
 
   AffineWrapper
-  (jsc [this ctx]
-    (let [[a b c d x y] (lang/aw-matrix this)
-          mag (falloleen.util/magnitude a b c d)]
-      (js* "var ctx=~{};ctx.save();ctx.transform(~{},~{},~{},~{},~{},~{});ctx.lineWidth=ctx.lineWidth/~{};"
-           ctx a c b d x y mag)
-      (jsc (lang/contents this) ctx)
-      (js* "~{}.restore();" ctx)))
+  (jsc [this]
+    (if-let [c (::code @(lang/cache this))]
+      c
+      (let [[a b c d x y] (lang/aw-matrix this)
+            mag (falloleen.util/magnitude a b c d)
+            code (str "ctx.save();"
+                      "ctx.transform(" a "," c "," b "," d "," x "," y ");"
+                      "ctx.lineWidth=ctx.lineWidth/" mag ";"
+                      (jsc (lang/contents this))
+                      "ctx.restore();")]
+        (swap! (lang/cache this) assoc ::code code)
+        code)))
 
   Line
-  (jsc [{[x1 y1] :from :as this} ctx]
-    (js* "~{}.moveTo(~{},~{});" ctx x1 y1)
-    (path-compile this ctx)
-    (js* "~{}.stroke();" ctx))
+  (jsc [{[x1 y1] :from :as this}]
+    (str "ctx.moveTo(" x1 "," y1 ");"
+         (path-compile this)
+         "ctx.stroke();"))
 
   Bezier
-  (jsc [{[x1 y1] :from :as this} ctx]
-    (js* "~{}.moveTo(~{},~{});" ctx x1 y1)
-    (path-compile this ctx)
-    (js* "~{}.stroke();" ctx))
+  (jsc [{[x1 y1] :from :as this}]
+    (str "ctx.moveTo(" x1 "," y1 ");"
+         (path-compile this)
+         "ctx.stroke()"))
 
   Spline
-  (jsc [{:keys [segments]} ctx]
+  (jsc [{:keys [segments]}]
     (let [[x y] (first (lang/boundary (first segments)))]
-      (js* "var ctx=~{};ctx.beginPath();ctx.moveTo(~{},~{});"
-           ctx x y)
-      (run! #(path-compile % ctx) segments)
-      (js* "~{}.stroke();" ctx)))
+      (str "ctx.beginPath();"
+           "ctx.moveTo(" x "," y ");"
+           (transduce (map path-compile) str "" segments)
+           "ctx.stroke();")))
 
   Style
-  (jsc [{:keys [style shape]} ctx]
-    (js* "~{}.save();" ctx)
-    (c-style style ctx)
-    (jsc shape ctx)
-    (js* "~{}.restore();" ctx)))
+  (jsc [{:keys [style shape]}]
+    (str "ctx.save();"
+         (c-style style)
+         (jsc shape)
+         "ctx.restore();")))
 
 (defn- clear-screen!
   "Clear the rendering context's canvas."
@@ -91,5 +95,6 @@
 
 (defn render! [shape ctx]
   (clear-screen! ctx)
-  (js* "~{}.fillStyle='rgba(0,0,0,0)';" ctx)
-  (jsc shape ctx))
+  ((js/eval (str "((ctx) => {"
+             "ctx.fillStyle='rgba(0,0,0,0)';"
+             (jsc shape) "})")) ctx))
